2f1dd761af4c0f24ab0c3e9bded4493251cdf1e3
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2023-11-13 21:40:14 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import IntroSection from './components/Intro'
+import ContactSection from './components/ContactSection'
+import MapSection from './components/Map' // import the map here
+import DisclaimerSection from './components/Disclaimer'
+import FooterSection from './components/Footer'
+
+import './App.css'
+
+const location = {
+  address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  lat: 37.42216,
+  lng: -122.08427,
+} // our location object from earlier
+
+const App = () => (
+  <div className="App">
+    <IntroSection />
+    <ContactSection />
+    <MapSection location={location} zoomLevel={17} /> {/* include it here */}
+    <DisclaimerSection />
+    <FooterSection />
+  </div>
+)
 
 export default App;
